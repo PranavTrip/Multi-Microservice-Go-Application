afc6ebe9039f43930b98239ed189dffb72fa100a
Pranav Tripathi <pranavtri00@gmail.com>
2025-05-18 15:04:31 +0530

    GraphQL

---
+++ graphql/query_resolver.go
@@ -0,0 +1,15 @@
+package main
+
+// import "context"
+
+type queryResolver struct {
+	server *Server
+}
+
+// func (r *queryResolver) Accounts(ctx context.Context, paginationInput *PaginationInput, id *string) ([]Account, error) {
+
+// }
+
+// func (r *queryResolver) Products(ctx context.Context, paginationInput *PaginationInput, query *string, id *string) ([]Product, error) {
+
+// }
