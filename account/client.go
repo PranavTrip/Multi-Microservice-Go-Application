4a6b283368d353a3634fef386cf2857c99973772
Pranav Tripathi <pranavtri00@gmail.com>
2025-06-06 15:59:41 +0530

    Account microservice

---
+++ account/client.go
@@ -0,0 +1,66 @@
+package account
+
+import (
+	"context"
+
+	"github.com/PranavTrip/go-grpc-graphql-ms/account/pb"
+	"google.golang.org/grpc"
+)
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service pb.AccountServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	conn, err := grpc.Dial(url, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	client := pb.NewAccountServiceClient(conn)
+	return &Client{conn, client}, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
+
+func (c *Client) PostAccount(ctx context.Context, name string) (*Account, error) {
+	res, err := c.service.PostAccount(ctx, &pb.PostAccountRequest{Name: name})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Account{
+		ID:   res.Account.Id,
+		Name: res.Account.Name,
+	}, nil
+}
+
+func (c *Client) GetAccount(ctx context.Context, id string) (*Account, error) {
+	res, err := c.service.GetAccount(ctx, &pb.GetAccountRequest{Id: id})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Account{
+		ID:   res.Account.Id,
+		Name: res.Account.Name,
+	}, nil
+}
+
+func (c *Client) GetAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error) {
+	res, err := c.service.GetAccounts(ctx, &pb.GetAccountsRequest{Skip: skip, Take: take})
+	if err != nil {
+		return nil, err
+	}
+	accounts := []Account{}
+	for _, a := range res.Accounts {
+		accounts = append(accounts, Account{
+			ID:   a.Id,
+			Name: a.Name,
+		})
+	}
+	return accounts, nil
+
+}
---
+++ account/repository.go
@@ -0,0 +1,80 @@
+package account
+
+import (
+	"context"
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+// Interface to be implemented for DB operations
+type Repository interface {
+	Close()
+	PutAccount(ctx context.Context, a Account) error
+	GetAccountByID(ctx context.Context, id string) (*Account, error)
+	ListAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error)
+}
+
+// Postgres Struct
+type postgresRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresRepository(url string) (Repository, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return &postgresRepository{db}, nil
+}
+
+func (r *postgresRepository) Close() {
+	r.db.Close()
+}
+
+func (r *postgresRepository) Ping() error {
+	return r.db.Ping()
+}
+
+func (r *postgresRepository) PutAccount(ctx context.Context, a Account) error {
+	// ExecContext for Create Operations
+	_, err := r.db.ExecContext(ctx, "INSERT INTO accounts(id, name) VALUES($1, $2)", a.ID, a.Name)
+	return err
+}
+
+func (r *postgresRepository) GetAccountByID(ctx context.Context, id string) (*Account, error) {
+	// QueryRowContext for Single Read Operations
+	row := r.db.QueryRowContext(ctx, "SELECT id,name FROM accounts WHERE id = $1", id)
+	a := &Account{}
+	if err := row.Scan(&a.ID, &a.Name); err != nil {
+		return nil, err
+	}
+	return a, nil
+}
+
+func (r *postgresRepository) ListAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error) {
+	//  QueryContext for Multiple Read Operations
+	rows, err := r.db.QueryContext(ctx, "SELECT id,name FROM accounts ORDER BY id DESC OFFSET $1 LIMIT $2", skip, take)
+	if err != nil {
+		return nil, err
+	}
+	accounts := []Account{}
+	defer rows.Close()
+
+	for rows.Next() {
+		a := &Account{}
+		if err = rows.Scan(&a.ID, &a.Name); err == nil {
+			accounts = append(accounts, *a)
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return accounts, nil
+
+}
